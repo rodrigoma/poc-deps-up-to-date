bc9de52d8c57086b78ff44360ad335856ca1a199
Rodrigo Montanha <roamaral@uolinc.com>
2021-12-20 11:43:06 -0300

    config files

---
+++ src/test/kotlin/com/rodrigoma/pocdepsuptodate/PocDepsUpToDateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rodrigoma.pocdepsuptodate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PocDepsUpToDateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
