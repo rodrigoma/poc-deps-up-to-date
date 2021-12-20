cf1155c74cd6490020c20c4228eda6f2c81c650f
rodrigoma <rodrigoma@gmail.com>
2021-12-20 17:04:39 +0000

    Upgraded org.jetbrains.kotlin:kotlin-stdlib-jdk8 from 1.3.40 to 1.6.10

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ enableFeaturePreview("VERSION_CATALOGS")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            alias("kotlin").to("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.40")
+            alias("kotlin").to("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.10")
             alias("kotlin-reflect").to("org.jetbrains.kotlin:kotlin-reflect:1.3.40")
             alias("kotlin-plugin").to("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.41")
             alias("spring-boot-starter").to("org.springframework.boot:spring-boot-starter:2.6.1")
