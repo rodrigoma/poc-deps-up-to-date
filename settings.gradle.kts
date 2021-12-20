e9d633650da71e34059611e8432361188ca654a7
rodrigoma <rodrigoma@gmail.com>
2021-12-20 17:04:39 +0000

    Upgraded org.jetbrains.kotlin:kotlin-gradle-plugin from 1.3.41 to 1.6.10

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ dependencyResolutionManagement {
         create("libs") {
             alias("kotlin").to("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.10")
             alias("kotlin-reflect").to("org.jetbrains.kotlin:kotlin-reflect:1.3.40")
-            alias("kotlin-plugin").to("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.41")
+            alias("kotlin-plugin").to("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
             alias("spring-boot-starter").to("org.springframework.boot:spring-boot-starter:2.6.1")
             alias("spring-boot-starter-test").to("org.springframework.boot:spring-boot-starter-test:2.6.1")
         }
