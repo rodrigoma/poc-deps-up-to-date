cf09d2bd64d3c6592c53ae7b31732da4a8d6304c
Rodrigo Monteiro do Amaral <rodrigoma@gmail.com>
2023-02-27 11:55:17 -0300

    Update PocDepsUpToDateApplication.kt

--- src/main/kotlin/com/rodrigoma/pocdepsuptodate/PocDepsUpToDateApplication.kt
+++ src/main/kotlin/com/rodrigoma/pocdepsuptodate/PocDepsUpToDateApplication.kt
@@ -6,6 +6,4 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class PocDepsUpToDateApplication
 
-fun main(args: Array<String>) {
-	runApplication<PocDepsUpToDateApplication>(*args)
-}
+fun main(args: Array<String>) {	runApplication<PocDepsUpToDateApplication>(*args) }
